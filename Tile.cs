4f8cdac86e52b7c9166102e22f4da073e20552a1
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-17 15:00:36 +0200

    feat: Link tiles and made board more visually pleasing. Add helper function to class Tile to display in id number

--- Tile.cs
+++ Tile.cs
@@ -4,24 +4,81 @@ record TileHolder(int Id, string Color, int NextId, int SpecialNExtId);
 class Tile
 {
 	public string Color = "NONE";
-	public int Id { get; set; }
-	public int NextId { get; set; }
+	public string Id { get; set; }
+	public string NextId { get; set; }
 	public int X { get; set; }
 	public int Y { get; set; }
-	public bool Visible = true;
+	public bool Special = false;
 	public Tile Next { get; set; }
 
-	public Tile(int y, int x, string color = "NONE")
+	public Tile(int y, int x, string id, string nextId, string color = "NONE", bool special = false)
 	{
 		X = x;
 		Y = y;
+		Id = id;
+		NextId = nextId;
 		//Color = color;
 		Color = color;
-		Next = null;
+		Special = special;
+		//Next = null;
 	}
 	public void DisplayTile()
 	{
-		if (Color != "NONE") MColoredText.GetColoredText(Color, "▢ ");
+		if (Special) MColoredText.GetColoredText(Color, "▨ ");
+		else if (Color != "NONE") MColoredText.GetColoredText(Color, "▢ ");
 		else Console.Write("▢ ");
 	}
+
+	// public void DisplayTile()
+	// {
+	// 	// Print 3 empty spaces for null spots to preserve column alignment
+	// 	if (this == null)
+	// 	{
+	// 		Console.Write("   ");
+	// 		return;
+	// 	}
+
+	// 	string display = Id;
+
+	// 	// Map entry tile names to track numbers
+	// 	if (display == "enterBlue") display = "1";
+	// 	else if (display == "enterGreen") display = "15";
+	// 	else if (display == "enterYellow") display = "29";
+	// 	else if (display == "enterRed") display = "43";
+
+	// 	// Map home areas
+	// 	else if (display == "homeRed") display = "R";
+	// 	else if (display == "homeBlue") display = "B";
+	// 	else if (display == "homeGreen") display = "G";
+	// 	else if (display == "homeYellow") display = "Y";
+
+	// 	// Strip prefixes and path designations
+	// 	else
+	// 	{
+	// 		display = display.Replace("tile", "")
+	// 						 .Replace("home", "")
+	// 						 .Replace("darkredPath", "")
+	// 						 .Replace("darkbluePath", "")
+	// 						 .Replace("darkgreenPath", "")
+	// 						 .Replace("darkyellowPath", "")
+	// 						 .Replace("Red", "")
+	// 						 .Replace("Blue", "")
+	// 						 .Replace("Green", "")
+	// 						 .Replace("Yellow", "");
+
+	// 		if (display == "center") display = "O";
+	// 	}
+
+	// 	// Format output: strictly 2 characters wide + 1 space separator
+	// 	string output = string.Format("{0,-2} ", display);
+
+	// 	if (Special || Color != "NONE")
+	// 	{
+	// 		MColoredText.GetColoredText(Color, output);
+	// 	}
+	// 	else
+	// 	{
+	// 		Console.Write(output);
+	// 	}
+	// }
 }
