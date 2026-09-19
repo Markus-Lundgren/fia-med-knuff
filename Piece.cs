b36318a432052daa9a858bdd47107749a3ad9030
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-19 15:39:14 +0200

    feat: Move home logic to separate method MoveHome in class Piece in order to reuse

--- Piece.cs
+++ Piece.cs
@@ -33,4 +33,104 @@ class Piece
 		}
 		//ANVÄND egen funktion
 	}
+
+	public void MoveHome(Board b)
+	{
+		Coords tmp = new();
+		switch (Color?.ToLower())
+		{
+			case "red":
+				switch (Id)
+				{
+					case 1:
+						tmp.Y = 1;
+						tmp.X = 1;
+						break;
+					case 2:
+						tmp.Y = 1;
+						tmp.X = 2;
+						break;
+					case 3:
+						tmp.Y = 2;
+						tmp.X = 1;
+						break;
+					case 4:
+						tmp.Y = 2;
+						tmp.X = 2;
+						break;
+				}
+				break;
+
+			case "blue":
+				switch (Id)
+				{
+					case 1:
+						tmp.Y = 1;
+						tmp.X = 12;
+						break;
+					case 2:
+						tmp.Y = 1;
+						tmp.X = 13;
+						break;
+					case 3:
+						tmp.Y = 2;
+						tmp.X = 12;
+						break;
+					case 4:
+						tmp.Y = 2;
+						tmp.X = 13;
+						break;
+				}
+				break;
+
+			case "green":
+				switch (Id)
+				{
+					case 1:
+						tmp.Y = 12;
+						tmp.X = 12;
+						break;
+					case 2:
+						tmp.Y = 12;
+						tmp.X = 13;
+						break;
+					case 3:
+						tmp.Y = 13;
+						tmp.X = 12;
+						break;
+					case 4:
+						tmp.Y = 13;
+						tmp.X = 13;
+						break;
+				}
+				break;
+
+			case "yellow":
+				switch (Id)
+				{
+					case 1:
+						tmp.Y = 12;
+						tmp.X = 1;
+						break;
+					case 2:
+						tmp.Y = 12;
+						tmp.X = 2;
+						break;
+					case 3:
+						tmp.Y = 13;
+						tmp.X = 1;
+						break;
+					case 4:
+						tmp.Y = 13;
+						tmp.X = 2;
+						break;
+				}
+				break;
+		}
+
+		Coords = tmp;
+		b.Tiles[Coords.Y, Coords.X]?.Pieces.Add(this);
+		Console.SetCursorPosition(2 + (Coords.X * 2), b.BoardTop + Coords.Y);
+		b.Tiles[Coords.Y, Coords.X]?.DisplayTile();
+	}
 }
